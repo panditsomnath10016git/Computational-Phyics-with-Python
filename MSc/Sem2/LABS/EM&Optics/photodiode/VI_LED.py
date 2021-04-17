76d8924f0ebf2199211055a3f7d26f8a6f1d147d
Somnath Pandit <panditsomnath10016@gmail.com>
2021-04-18 00:49:56 +0530

    photodiode

--- MSc/Sem2/LABS/EM&Optics/photodiode/VI_LED.py
+++ MSc/Sem2/LABS/EM&Optics/photodiode/VI_LED.py
@@ -2,14 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # import data from file
-data = np.loadtxt("red.txt")
-v_data = data[:,0]
-i_data = data[:,1]
+data = np.loadtxt("green.txt")
+v_data = data[:, 0]
+i_data = data[:, 1]
 
 plt.plot(v_data, i_data, ".-g")
-plt.title("V - I characteristics of red LED")
+plt.title("V - I characteristics of green LED")
 plt.xlabel("Bias voltage (V) $\longrightarrow$")
 plt.ylabel("Photocurrent (I)$(\mu A)\longrightarrow$")
 plt.grid()
-plt.savefig("red.pdf")
+plt.tight_layout()
+plt.savefig("green.pdf")
 plt.show()
--- MSc/Sem2/LABS/EM&Optics/photodiode/VI_photodiode.py
+++ MSc/Sem2/LABS/EM&Optics/photodiode/VI_photodiode.py
@@ -2,14 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # import data from file
-data = np.loadtxt("i2.txt")
-v_data = data[:,0]
-i_data = data[:,1]
+data1 = np.loadtxt("i1.txt") * -1
+data2 = np.loadtxt("i2.txt") * -1
+v_data1 = data1[:, 0]
+i_data1 = data1[:, 1]
+v_data2 = data2[:, 0]
+i_data2 = data2[:, 1]
 
-plt.plot(v_data, i_data, ".-")
-plt.title("V - I characteristics of photdiode with I2 intensity")
-plt.xlabel("Bias voltage (V) $\longrightarrow$")
-plt.ylabel("Photocurrent (I)$(\mu A)\longrightarrow$")
+
+plt.plot(v_data1, i_data1, ".-", label="I1")
+plt.plot(v_data2, i_data2, ".-", label="I2<I1")
+plt.title("V - I characteristics of photdiode at I1 & I2<I1 intensity")
+plt.xlabel("Reverse bias voltage (V) $\longrightarrow$")
+plt.ylabel("Reverse photocurrent (I)$(\mu A)\longrightarrow$")
 plt.grid()
-plt.savefig("i2.pdf")
+plt.legend(loc="best")
+plt.savefig("photodiode.pdf")
 plt.show()
